35329200ff36c4642370802b677bfd4429726473
Ruslan Priadko <r.priadko@ukr.net>
2025-08-27 15:07:34 +0300

    Add Task and Teg models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks",
+    )
+
+    def __str__(self):
+        return (
+            f"{self.content}, tags: {self.tags.objects.all()}"
+        )
